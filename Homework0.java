af2cebe25dcea568b4ba81ac08635f9ed2249a53
Reece Boulware <boulwree@berkeleyprep.org>
2018-08-27 08:53:47 -0400

    hi

--- Homework0.java
+++ Homework0.java
@@ -1,15 +1,14 @@
 /*
- * First and Last Names
- */
+ *First and Last Name
+*/
 
 public class Homework0 {
-
 	/*
 	 * Print out the line "Hello, World!"
 	 * Ensure that you match case and punctuation exactly
 	 */
 	public static void main(String[] args) {
-
+    System.out.println("Hello, World!");
 	}
 
 }
